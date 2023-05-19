3af6c006d4045801b8f0d92c014165231a502049
Juan Luis Mingo Moreno <juanluismingomoreno@MacBook-Air-de-Juan.local>
2023-05-18 20:18:01 -0400

    tarea

--- app.js
+++ app.js
@@ -1,59 +1,97 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const BigNumber = require('bignumber.js');
+const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3001;
-
-app.get("/", (req, res) => res.type('html').send(html));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(bodyParser.json());
 
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+
+app.set('view engine', 'ejs'); // Set EJS as the template engine
+app.set('views', __dirname);
+
+
+
+let numero_de_transacciones = 0;
+let resumen_operaciones = {
+          "monto_2200" : 0,
+          "numero_2200" : 0,
+          "monto_2400" : 0,
+          "numero_2400" : 0
+};
+
+
+const receivedTransactions = {};
+
+app.post('', (req, res) => {
+  const requestBody = req.body;
+  const base64Number = requestBody["message"]["data"];
+
+  function base64ToBase10(base64Number1) {
+    const decodedString = Buffer.from(base64Number1, 'base64').toString('utf-8');
+    const base10Number = BigInt(decodedString).toString();
+    return base10Number;
+  }
+
+  const base10Number = base64ToBase10(base64Number);
+
+  var tipo_operacion = base10Number.substring(0, 4);
+  var id_mensaje = base10Number.substring(4, 14);
+  var banco_origen = base10Number.substring(14, 21);
+  var cuenta_origen = base10Number.substring(21, 31);
+  var banco_destino = base10Number.substring(31, 38);
+  var cuenta_destino = base10Number.substring(38, 48);
+  var monto = base10Number.substring(48, 64);
+
+  var nueva_transaccion = {
+    "tipo_operacion": tipo_operacion,
+    "banco_origen": banco_origen,
+    "cuenta_origen": cuenta_origen,
+    "banco_destino": banco_destino,
+    "cuenta_destino" : cuenta_destino,
+    "monto": parseInt(monto)
+  };
+
+  
+
+  //console.log(receivedTransactions);
+  console.log(nueva_transaccion);
+
+
+
+
+  if (!receivedTransactions[id_mensaje]) {
+    // La transacción es nueva
+    receivedTransactions[id_mensaje] = nueva_transaccion;
+
+    if (nueva_transaccion["tipo_operacion"] === "2200") {
+      resumen_operaciones["monto_2200"] += nueva_transaccion["monto"];
+      resumen_operaciones["numero_2200"] += 1;
+    }
+    if (nueva_transaccion["tipo_operacion"] === "2400") {
+      resumen_operaciones["monto_2400"] += nueva_transaccion["monto"];
+      resumen_operaciones["numero_2400"] += 1;
+    }
+
+
+  }
+
+  console.log(resumen_operaciones);
+  numero_de_transacciones = Object.keys(receivedTransactions).length;
+  res.status(200);
+});
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', { transactions: receivedTransactions , operaciones: numero_de_transacciones, resumen_operaciones: resumen_operaciones}); // Renderiza la plantilla y pasa los datos de receivedTransactions
+});
+
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor API escuchando en el puerto ${port}`);
+});
+
+
+
